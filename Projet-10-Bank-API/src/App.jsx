cc4dd398d2f4294a32841fc722ad4d1a37f5b8e2
dorea <dodomokhtari@hotmail.fr>
2024-12-27 18:34:10 +0100

    home page

--- Projet-10-Bank-API/src/App.jsx
+++ Projet-10-Bank-API/src/App.jsx
@@ -1,28 +1,20 @@
-import './App.css'
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './store';
-import Home from './Home';
-import Login from './Login';
 
-const root = createRoot(document.getElementById('root'));
-
-root.render(
-  <Provider store={store}>
-    <Router>
-      <Routes> <Route path="/Home" element={<Home />} />
-        <Route path="/Login" element={<Login />} />
-      </Routes>
-    </Router>
-  </Provider>
-);
-
-
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+import Home from "./Home";
+import "./App.css";
 
 function App() {
-
+  return (
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </Provider>
+  );
 }
 
-export default App
+export default App;
---
+++ Projet-10-Bank-API/src/Home.jsx
@@ -0,0 +1,80 @@
+import './assets/img/bank-tree.jpeg';
+import argentbanklogo from './assets/img/argentBankLogo.webp';
+import iconchat from './assets/img/icon-chat.webp';
+import iconsecurity from './assets/img/icon-security.webp';
+import iconmoney from './assets/img/icon-money.webp';
+import './assets/css/main.css';
+
+
+function Home() {
+  return (
+    <>
+      <nav className="main-nav">
+        <a className="main-nav-logo" href="/">
+          <img
+            className="main-nav-logo-image"
+            src={argentbanklogo}
+            alt="Argent Bank Logo"
+          />
+          <h1 className="sr-only">Argent Bank</h1>
+        </a>
+        <div>
+          <a className="main-nav-item" href="/login">
+            <i className="fa fa-user-circle"></i>
+            Sign In
+          </a>
+        </div>
+      </nav>
+      <main>
+        <div className="hero">
+          <section className="hero-content">
+            <h2 className="sr-only">Promoted Content</h2>
+            <p className="subtitle">No fees.</p>
+            <p className="subtitle">No minimum deposit.</p>
+            <p className="subtitle">High interest rates.</p>
+            <p className="text">Open a savings account with Argent Bank today!</p>
+          </section>
+        </div>
+        <section className="features">
+          <h2 className="sr-only">Features</h2>
+          <div className="feature-item">
+            <img src={iconchat} alt="Chat Icon" className="feature-icon" />
+            <h3 className="feature-item-title">You are our #1 priority</h3>
+            <p>
+              Need to talk to a representative? You can get in touch through our
+              24/7 chat or through a phone call in less than 5 minutes.
+            </p>
+          </div>
+          <div className="feature-item">
+            <img
+              src={iconmoney}
+              alt="Money Icon"
+              className="feature-icon"
+            />
+            <h3 className="feature-item-title">More savings means higher rates</h3>
+            <p>
+              The more you save with us, the higher your interest rate will be!
+            </p>
+          </div>
+          <div className="feature-item">
+            <img
+              src={iconsecurity}
+              alt="Security Icon"
+              className="feature-icon"
+            />
+            <h3 className="feature-item-title">Security you can trust</h3>
+            <p>
+              We use top of the line encryption to make sure your data and money
+              is always safe.
+            </p>
+          </div>
+        </section>
+      </main>
+      <footer className="footer">
+        <p className="footer-text">Copyright 2020 Argent Bank</p>
+      </footer>
+    </>
+  );
+}
+
+export default Home;
