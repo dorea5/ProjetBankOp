3b7eb70e549927b38fdcd2f6d2e23c7df205ae0e
dorea <dodomokhtari@hotmail.fr>
2024-12-31 12:06:43 +0100

    fontawesome

--- Projet-10-Bank-API/src/User.jsx
+++ Projet-10-Bank-API/src/User.jsx
@@ -1,11 +1,33 @@
-import Header from "./Header";
+
 import Footer from "./Footer";
 import "./assets/css/main.css";
+import argentbanklogo from './assets/img/argentBankLogo.webp';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons';
+
+
 
 function User() {
   return (
     <>
-      <Header />
+      <nav className="main-nav">
+        <a className="main-nav-logo" href="/">
+          <img
+            className="main-nav-logo-image"
+            src={argentbanklogo}
+            alt="Argent Bank Logo"
+          />
+          <h1 className="sr-only">Argent Bank</h1>
+        </a>
+        <div>
+          <a className="main-nav-item" href="/sign-out">
+            <i className="fa fa-user-circle"></i>
+            <FontAwesomeIcon icon={faArrowRightFromBracket} color="grey" className="iconspace" />
+
+            Sign out
+          </a>
+        </div>
+      </nav>
       <main className="main bg-dark">
         <div className="header">
           <h1>
